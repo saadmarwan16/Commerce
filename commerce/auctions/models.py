745caf778edbe8dd63d86d65c0af6b20f0d65e0c
Marwan Sa-ad <saadmarwan16@gmail.com>
2020-12-21 16:22:09 +0300

    Users can now see all the listings they have created

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -15,7 +15,7 @@ class AuctionListings(models.Model):
     last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     time_auctioned = models.DateTimeField(auto_now=False, auto_now_add=True)
     image = models.ImageField(upload_to="listing-image", blank=True, null=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctioned_items")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctioned_items")
 
     def __str__(self):
         return f"{self.user_id} posted {self.name} for {self.price} at {self.time_auctioned}"
@@ -24,8 +24,8 @@ class AuctionListings(models.Model):
 class Bids(models.Model):
     price = models.DecimalField(max_digits=64, decimal_places=2)
     time_bid = models.DateTimeField(auto_now=False, auto_now_add=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
-    auction_id = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
+    auction = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_bids")
 
     def __str__(self):
         return f"{self.user_id} wants {self.auction_id} for {self.price} at {self.time_bid}"
@@ -34,8 +34,8 @@ class Bids(models.Model):
 class Comments(models.Model):
     content = models.TextField()
     time_commented = models.DateTimeField(auto_now=False, auto_now_add=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
-    auction_id = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
+    auction = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_comments")
 
     def __str__(self):
         return f"{self.user_id} commented {self.content} on {self.auction_id} at {self.time_commented}"
@@ -43,8 +43,8 @@ class Comments(models.Model):
 
 class WatchList(models.Model):
     time_watchlisted = models.DateTimeField(auto_now=False, auto_now_add=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_watchlisted_items")
-    auction_id = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_watchlists")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_watchlisted_items")
+    auction = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="item_watchlists")
 
     def __str__(self):
         return f"{self.user_id} watchlisted {self.auction_id} at {self.time_watchlisted}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -7,12 +7,15 @@ from django.urls import reverse
 from .models import User, AuctionListings, Bids, Comments, WatchList
 
 
+# Home page, this page displays the current active listings and their information
 def index(request):
     return render(request, "auctions/index.html", {
         "listings": AuctionListings.objects.all()
     })
 
 
+# Either shows a login page or takes a post request from login page and the log the user in if the details
+# are correct
 def login_view(request):
     if request.method == "POST":
 
@@ -33,11 +36,13 @@ def login_view(request):
         return render(request, "auctions/login.html")
 
 
+# Logs the user out and then return the user back to the home page
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
+# Either shows the register page of register a new user throug a post request
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -59,25 +64,15 @@ def register(request):
             return render(request, "auctions/register.html", {
                 "message": "Username already taken."
             })
+
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
 
 
+# Either shows the creating listing page to create a new listing or create a new listing
 def create_listing(request):
-    if request.method == "POST":
-        name = request.POST["title"]
-        description = request.POST["description"]
-        category = request.POST["category"]
-        price = request.POST["bid"]
-        image = request.POST["image"]
-        
-        listing = AuctionListings(name=name, description=description, category=category, price=price)
-        listing.save()
-
-        return render("auctions/listing-created.html")
-
     return render(request, "auctions/create-listing.html")
 
 
@@ -89,15 +84,29 @@ def watchlist(request):
     return render(request, "auctions/watchlist.html")
 
 
-def listing(request, num):
-    return render(request, "actions/listing.html")
+def listing(request):
+    return render(request, "auctions/listing.html")
 
 
-def listing_created(request):
+def listing_created(request, user_id):
+    name = request.POST["title"]
+    description = request.POST["description"]
+    category = request.POST["category"]
+    price = request.POST["bid"]
+    image = request.POST["image"]
+
+    # Get the user who just created the listing
+    print(user_id)
+    user = User.objects.get(pk=user_id)
+    
+    listing = AuctionListings(name=name, description=description, category=category, price=price, image=image, user=user)
+    listing.save()
+
     return render(request, "auctions/listing-created.html")
 
 
+# Show all the listings that a user has created
 def user_listing(request, id):
     return render(request, "auctions/user-listings.html", {
-        "listings": AuctionListings.objects.all(user_id=id)
+        "listings": AuctionListings.objects.get(user_id=id)
     })
